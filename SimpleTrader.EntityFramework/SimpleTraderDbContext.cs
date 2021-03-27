a703bbc55758585c4ddbb64229f594070a74852f
sanderdebr <sander606@gmail.com>
2021-03-27 10:44:03 +0100

    migrations

--- SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -12,6 +12,18 @@ namespace SimpleTrader.EntityFramework
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AssetTransaction> AssetTransactions { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Embed stock inside AssetTransaction table
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
 
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleTraderDB;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
