2a4d3a115888d9e391a68b4b7eac183c4f8cb05a
sanderdebr <sander606@gmail.com>
2021-03-27 13:59:49 +0100

    added API service

---
+++ SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrepAPI.Services
+{
+    public class MajorIndexService : IMajorIndexService
+    {
+        public async Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string uri = "https://financialmodelingprep.com//api/v3/majors-indexes/.DJI" + GetUriSuffix(indexType);
+
+                HttpResponseMessage response = await client.GetAsync($"{uri}?apiKey=e4b49a28379da98c0caf6cec14bdc989");
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                MajorIndex majorIndex = JsonConvert.DeserializeObject<MajorIndex>(jsonResponse);
+                majorIndex.Type = indexType;
+                return majorIndex;
+            }
+        }
+
+        private string GetUriSuffix(MajorIndexType indexType)
+        {
+            switch (indexType)
+            {
+                case MajorIndexType.DowJones:
+                    return ".DJI";
+                case MajorIndexType.Nasdaq:
+                    return ".IXIC";
+                case MajorIndexType.SP500:
+                    return ".INX";
+                default:
+                    return ".DJI";
+            }
+        }
+    }
+}
--- SimpleTrader.WPF/ViewModels/HomeViewModel.cs
+++ SimpleTrader.WPF/ViewModels/HomeViewModel.cs
@@ -6,5 +6,11 @@ namespace SimpleTrader.WPF.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        public MajorIndexViewModel MajorIndexViewModel { get; set; }
+
+        public HomeViewModel(MajorIndexViewModel majorIndexViewModel)
+        {
+            MajorIndexViewModel = majorIndexViewModel;
+        }
     }
 }
